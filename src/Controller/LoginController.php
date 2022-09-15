5b9a8ca5a271c1f6f4befc5baf92dd27be2f28c2
ndelgado <ndelgado@hiberus.com>
2022-09-15 08:56:27 +0200

    OPtimizacion de codigo y eliminacion platillas/controladores que no se usan

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,36 +2,27 @@
 
 namespace App\Controller;
 
-use App\Form\LoginFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
-                 $error = $authenticationUtils->getLastAuthenticationError();
-                // last username entered by the user
-                 $lastUsername = $authenticationUtils->getLastUsername();
-                /* $formLogin = $this->createForm(LoginFormType::class);
-                 if($formLogin->isSubmitted() && $formLogin->isValid()){
-                     return $this->redirectToRoute('app_login');
-                 }*/
-
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
-            'last_username' => $lastUsername,
-            'error'         => $error,
-            //'formLogin' => $formLogin->createView()
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('login2/index.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername
         ]);
     }
 
-    #[Route('/logout', name: 'fsgsfg')]
-    public function logoghgfhut(AuthenticationUtils $authenticationUtils): Response
+    #[Route('/logout2', name: 'app_logout2')]
+    public function logout(AuthenticationUtils $authenticationUtils): Response
     {
     }
 }
